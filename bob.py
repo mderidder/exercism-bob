c3069fd785a497d67c479e9d5a55a91560b5308b
Michael de Ridder <deridder.michael@gmail.com>
2015-01-02 14:27:57 -0500

    added support for yelling and silence

--- bob.py
+++ bob.py
@@ -2,5 +2,14 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 def hey(what):
-    responses = {'','Whatever.'}
-    return 'Whatever.'
+    responses = {'': 'Whatever.',
+                 'silence': 'Fine. Be that way!',
+                 'upper': 'Whoa, chill out!'}
+    what = what.strip()
+    state = ''
+    if what.isupper():
+        state = 'upper'
+    if what == '':
+        state = 'silence'
+
+    return responses[state]

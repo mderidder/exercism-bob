afa0536df47611196b6c94bc40094190fe9a8ad0
Michael de Ridder <deridder.michael@gmail.com>
2015-01-02 14:36:12 -0500

    added support for asking question

--- bob.py
+++ bob.py
@@ -3,10 +3,15 @@
 #
 def hey(what):
     responses = {'': 'Whatever.',
+                 'question': 'Sure.',
                  'silence': 'Fine. Be that way!',
                  'upper': 'Whoa, chill out!'}
+
     what = what.strip()
     state = ''
+
+    if what.endswith('?'):
+        state = 'question'
     if what.isupper():
         state = 'upper'
     if what == '':
